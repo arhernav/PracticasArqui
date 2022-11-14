49c72b4f9e763c3c139d6777d45e0d019dcb1014
Armando Hernández Navarro <arhernav@ciencias.unam.mx>
2022-11-13 22:50:16 -0600

    Ejercicio 4 esta casi listo

--- Practica6/Ejercicio4-mips.asm
+++ Practica6/Ejercicio4-mips.asm
@@ -1,25 +1,61 @@
 # Hernández Navarro Armando - 317340347
 # Juan Carlos Zenteno Pompa - 316251608
 .data
-	
+	n: .word 100
+.text
 
-.text 
-	## Inicio del programa
 	main:
 	
+	lw $a1, n #Guardamos n en $f2
+	add $a2, $zero, 0 #Guardamos una variable para iteraciones
+	jal while #Llamamos al while 
 	
-		jal ejercicio4
-		
-		## Finalización del programa. Sin esta linea, el programa nunca termina
-		li $v0, 10
-		syscall 
-		
+	#li $v0, 10
+	#syscall 
 	
-	#Declaración de la funcion
 	
-	#Copia el valor del registro $a1 al registro $v0
-	ejercicio4: 
+	while:
+	# while($a2 = 0 < $a1 = n) 
+	# $a3 usado como ayuda para operaciones
+	# Usamos $f4 para saber si es 1 o -1 en el dividendo
+	# Llevaremos el resultado en $f3
+		bge  $a2,  $a1, exit
+		add $t7, $zero, 2
+		div $a2, $t7 #Obtenemos division
+		mfhi $a3 #Movemos el  modulo de  la division
+		beqz $a3, par #Usamos a3 como condicional
+		beq  $a3, 1, impar #Usamos a3 como condicional
+
+		j while #Volvemos al ciclo
 		
-	
-		jr $ra
 		
+	par:
+		add $a3, $zero, 1 #Vuelve a3 = 1 
+		addi  $t1, $zero, 2 #Guardamos 2 como constante en t1
+		mult $a2, $t1 #Realizamos 2n
+		mflo $t2 #Guardamos el resultado de la operacion en t2
+		add $a3, $a3, $t2 #Sumamos $a3 = 1 con 2n. 2n + 1
+		addi $t4, $zero, 1 #Ya que es par, usamos 1 para division
+		div $t5, $t4, $a3 #Realizamos la division y la guardamos en t5
+		add $v1, $v1, $t5 #Sumamos $t5 con lo que llevabamos
+		
+		add $a2, $a2, 1 #Añadimos una iteracion al ciclo#
+		j while
+		
+		
+	impar:			
+		add $a3, $zero, 1 #Vuelve a3 = 1 
+		addi  $t1, $zero, 2 #Guardamos 2 como constante en t1
+		mult $a2, $t1 #Realizamos 2n
+		mflo $t2 #Guardamos el resultado de la operacion en t2
+		add $a3, $a3, $t2 #Sumamos $a3 = 1 con 2n. 2n + 1
+		addi $t4, $zero, -1 #Ya que es imparpar, usamos 1 para division
+		div $t5, $t4, $a3 #Realizamos la division y la guardamos en t5
+		add $v1, $v1, $t5 #Sumamos $t5 con lo que llevabamos
+		
+		add $a2, $a2, 1 #Añadimos una iteracion al ciclo#
+		j while
+		
+		
+	exit:
+		add $v1, $a2, $zero #Guardamos el valor en $v0 y terminamos
